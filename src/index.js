90aae723bf02d6beefab94f239e60e874c28f7d8
leshakrikun <lesha.krikun@users.noreply.github.com>
2021-05-16 18:04:31 +0300

    before-check

--- src/index.js
+++ src/index.js
@@ -2,25 +2,50 @@ import './sass/main.scss';
 import dish from "./menu.json"
 import dishesTemplate from './templates/dishes.hbs'
 
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
 const dishesData = {
     dish: dish,
 }
-console.log(dish)
+
 const dishesHTML = dishesTemplate(dishesData)
-console.log(dishesHTML)
 document.querySelector('.js-menu').innerHTML = dishesHTML 
 
-/* 
-const listDishes = menu.map(menu => {
-    const newEl = `<li class = 'gallery__item'><a class = 'gallery__link' href = "${image.original}"><img class = 'gallery__image' src = ${image.preview} data-source = ${image.original} alt=${image.description}></a></li>`
-    return newEl
-})
+const currentTheme = document.querySelector('body');
+const themeSwitcher = document.querySelector('#theme-switch-toggle');
 
 
 
-"id": "XWaQXcbk0",
-"name": "Картофель, запеченный в мундире",
-"description": "Ароматный, сытный, шипящий домашний картофель, фаршированный сметанно-беконной начинкой, — это очень простой и очень эффектный способ накормить большое количество человек, практически не потратив на готовку ни сил, ни времени. Обычную картошку при желании тут можно заменить на сладкий батат, а в начинку добавить, к примеру, кукурузу или сладкий красный перец.",
-"image": "https://s1.eda.ru/StaticContent/Photos/140812180013/140820212258/p_O.jpg",
-"price": 100,
-"ingredients": [ */
+const lightThemeFunc = () => {
+    currentTheme.classList.add(Theme.LIGHT);
+    currentTheme.classList.remove(Theme.DARK);
+    localStorage.removeItem('theme');
+    localStorage.setItem('theme', Theme.LIGHT);
+};
+
+const darkThemeFunc = () => {
+    currentTheme.classList.add(Theme.DARK);
+    currentTheme.classList.remove(Theme.LIGHT);
+    localStorage.removeItem('theme');
+    localStorage.setItem('theme', Theme.DARK);
+};
+
+const changeThemeFunc = () => {
+    if (!themeSwitcher.checked) {
+        lightThemeFunc();
+    } else {
+        darkThemeFunc();
+    }
+};
+
+themeSwitcher.addEventListener('change', changeThemeFunc);
+
+if (localStorage.getItem('theme') === Theme.DARK) {
+  themeSwitcher.checked = 'true';
+  darkThemeFunc();
+}
+
+
